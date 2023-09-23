7e61f3c33fc77d8ea4b1c6e4cceeb2f6dc6adfc2
BrianoAden <brianoeaden@gmail.com>
2023-09-22 23:22:57 -0500

    remake roll function

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import random
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix = commands.when_mentioned_or('!'), intents = intents)
@@ -13,5 +14,13 @@ async def on_ready():
 async def main():
 	await bot.start('MTE1NDk3MDY0NTQxMTY3MjA3NA.G_Jvl3.f9i2WY3XIrPRDvqyX2oHn0xdjQh5TgbuzhyuKg')
 asyncio.run(main())
+
+async def roll(n):
+    """
+    Rolls a n-sided dice.
+    Inputs:
+    n = integer representing the number of sides of the die.
+    """
+    return str(random.randint(1,n))
 
 
--- cogs/misc.py
+++
@@ -1,9 +0,0 @@
-import random 
-
-def roll(n):
-    """
-    Rolls a n-sided dice.
-    Inputs:
-    n = integer representing the number of sides of the die.
-    """
-    return str(random.randint(1,n))
