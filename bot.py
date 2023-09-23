4dd217c977f49d40dc8fa9d0fb179d50feed646c
angel-romero-f <angelfloromero@gmail.com>
2023-09-23 00:50:18 -0500

    Implement roll in cogs.misc

--- bot.py
+++ bot.py
@@ -13,16 +13,11 @@ async def on_ready():
 	print('------')
 
 async def main():
+	await bot.load_extension('cogs.misc')
 	await bot.start('MTE1NDk5NjUyNzA3MzM0NTU4Nw.Ga9JEb.2v5fCD73nONnyg7SPXfZ9ImxkE2Yv2rRggGxGI')
+	
 
-@bot.command()
-async def roll(ctx: commands.Context, num_rolls: int):
-    """
-    Rolls a n-sided dice.
-    Inputs:
-    n = integer representing the number of sides of the die.
-    """
-    await ctx.send(f"You rolled a {random.randrange(1,num_rolls + 1)}")
+
 
 asyncio.run(main())
 
