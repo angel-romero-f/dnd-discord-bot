cbb3b61f2d460ef6aa3cf0b491fa89d38dc33076
Noah <nv16@rice.edu>
2023-09-22 23:56:45 -0500

    added messaging functionality

--- bot.py
+++ bot.py
@@ -11,6 +11,13 @@ async def on_ready():
 	print(f'Logged in as {bot.user} (ID: {bot.user.id})')
 	print('------')
 
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+    	if message.content.startswith('!roll'):
+        	# Process the roll command
+        	await message.channel.send("Rolling the dice...")
+
 async def main():
 	await bot.start('MTE1NDk5NjUyNzA3MzM0NTU4Nw.Ga9JEb.2v5fCD73nONnyg7SPXfZ9ImxkE2Yv2rRggGxGI')
 	
