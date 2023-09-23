614a70c71d302fdbbe2a9fd5d8a56cdc8051beda
angel-romero-f <angelfloromero@gmail.com>
2023-09-23 00:09:41 -0500

    Fixed problems

--- cogs/misc.py
+++ cogs/misc.py
@@ -4,13 +4,3 @@ from discord.ext import commands
 import asyncio
 
 
-@commands.command(name = 'roll',
-                  brief = 'roll an n sided dice')
-
-async def roll(ctx: commands.Context, num_sides: int):
-    """
-    Rolls a n-sided dice.
-    Inputs:
-    n = integer representing the number of sides of the die.
-    """
-    await ctx.send(f"You rolled {random.randrange(1,num_sides + 1)}")
