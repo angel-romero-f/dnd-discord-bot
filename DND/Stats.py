129bd607a115567dcc46fe84e0fff5b25ce0c642
BrianoAden <brianoeaden@gmail.com>
2023-09-23 13:49:11 -0500

    fix error in statroll func

--- DND/Stats.py
+++ DND/Stats.py
@@ -9,7 +9,7 @@ class Stats(Character):
     Should initalize with the information of level, race, and class that way it knows what to make each stat and how to modify it
     Making use of methods like roll may be useful to create methods in here, should probably br able to display
     """
-    def __init__(self, hit_points, strength, cons, dext, wis, intelligence, charisma, char_lvl):
+    def __init__(self, hit_points, strength, constitution, dexterity, wisdom, intelligence, charisma, char_lvl):
         self.health_points = hit_points
         self.strength = strength 
         self.constitution = constitution
