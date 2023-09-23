bb7c560bc767f7cf65584e4d2cbef6b3d36ee132
angel-romero-f <angelfloromero@gmail.com>
2023-09-23 11:21:13 -0500

    Implement new_char and char_name in player.py

--- cogs/player.py
+++ cogs/player.py
@@ -6,4 +6,16 @@ class Player(commands.Cog):
     character_ids = {}
 
     def __init__(self, client):
-        pass
+        self.client = client
+
+    @commands.command(name = 'new_char')
+    async def new_char(self, ctx: commands.Context, name):
+        self.character_ids[ctx.author] = name
+        await ctx.send(f'hello {name}!' )
+
+    @commands.command(name = "char_name")
+    async def char_name(self, ctx: commands.Context):
+        await ctx.send(self.character_ids[ctx.author])
+
+async def setup(client):
+    await client.add_cog(Player(client))
