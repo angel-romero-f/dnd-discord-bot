f867dd9e3eeac2d2dd5ff34e79e53c092e82c8fa
Noah <nv16@rice.edu>
2023-09-23 11:45:07 -0500

    Added spell slot alterations

--- DND/Bard.py
+++ DND/Bard.py
@@ -5,6 +5,13 @@ class Bard(Class):
     like level up to allow for more class specific results
     this is where attributes of this class should be held
     """
-    def __init__(self, class_name, hit_points, class_features, level=1, instrument=None):
+    def __init__(self, class_name, hit_points, class_features, slots, level=1, instrument=None):
         super().__init__(class_name, hit_points, class_features, level)
         self.instrument = instrument
+        self.slots = slots
+    
+    def alter_slots(self, level, change):
+        a = self.slots[level]
+        self.slots[level] = a+change
+
+    
