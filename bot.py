2fb62e03d31a6ac4376f858ca48490022356d3a5
angel-romero-f <angelfloromero@gmail.com>
2023-09-22 23:20:43 -0500

    Put bot.py back

--- bot.py
+++ bot.py
@@ -11,15 +11,7 @@ async def on_ready():
 	print('------')
 
 async def main():
-	try:
-		await bot.load_extension("cogs.rps")
-		await bot.load_extension("cogs.util")
-		print(f'Extension loaded!')
-	except Exception as e:
-		print(f'Failed to load extension cogs.')
-		print(str(e))
 	await bot.start('MTE1NDk3MDY0NTQxMTY3MjA3NA.G_Jvl3.f9i2WY3XIrPRDvqyX2oHn0xdjQh5TgbuzhyuKg')
-
 asyncio.run(main())
 
 
