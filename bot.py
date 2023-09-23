9116da1a114708501aeda2f303d1dd52fa24cc56
Noah <nv16@rice.edu>
2023-09-22 23:52:41 -0500

    start

--- bot.py
+++ bot.py
@@ -12,13 +12,6 @@ async def on_ready():
 	print('------')
 
 async def main():
-	try:
-		await bot.load_extension("cogs.rps")
-		await bot.load_extension("cogs.util")
-		print(f'Extension loaded!')
-	except Exception as e:
-		print(f'Failed to load extension cogs.')
-		print(str(e))
 	await bot.start('MTE1NDk5NjUyNzA3MzM0NTU4Nw.Ga9JEb.2v5fCD73nONnyg7SPXfZ9ImxkE2Yv2rRggGxGI')
 	
 asyncio.run(main())
