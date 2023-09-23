ccd8ab1436f1d1ef7491356dc6e0481c26c403d0
Noah <nv16@rice.edu>
2023-09-23 05:45:58 -0500

    Added to roll function

--- cogs/misc.py
+++ cogs/misc.py
@@ -8,13 +8,30 @@ class Misc(commands.Cog):
         self.client = client
     @commands.command(name = 'roll',
                       brief = 'rolls a fair n sided die')
-    async def roll(self, ctx: commands.Context, num_sides: int):
+    async def roll(self, ctx: commands.Context, num_sides: int, num_dice: int = 1, advantage: bool = False):
         """
         Rolls a n-sided dice.
         Inputs:
-        n = integer representing the number of sides of the die.
+        num_sides = integer representing the number of sides of the die (required).
+        num_dice = integer representing the number of dice rolled (optional, default is 1).
+        advantage = boolean flag indicating whether it's an advantage roll (optional, default is False).
+        Outputs: A dice roll that is modified by the number of dice rolled, the number of faces, and 
+        whether or not we use advantage
         """
-        await ctx.send(f"You rolled a {random.randrange(1,num_sides + 1)}")
+        if num_sides <= 0 or num_dice <= 0:
+            await ctx.send("Please provide positive values for the number of dice and sides.")
+            return
+
+        if advantage:
+            # Advantage roll - roll two dice and take the higher result
+            rolls = [random.randint(1, num_sides) for _ in range(2 * num_dice)]
+            results = [max(rolls[i], rolls[i + 1]) for i in range(0, len(rolls), 2)]
+        else:
+            # Regular roll - roll the specified number of dice
+            results = [random.randint(1, num_sides) for _ in range(num_dice)]
+
+        result_str = f'Advantage Roll: {", ".join(map(str, results))}' if advantage else f'Roll: {", ".join(map(str, results))}'
+        await ctx.send(result_str)
 
 async def setup(client):
     await client.add_cog(Misc(client))
