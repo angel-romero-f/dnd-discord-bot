94d56bdd58712ab8a3ba85442090ea90dce76a57
Noah <nv16@rice.edu>
2023-09-23 00:53:31 -0500

    class constructors

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,4 +3,18 @@ import discord
 from discord.ext import commands
 import asyncio
 
+class Misc(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    @commands.command(name = 'roll',
+                      brief = 'rolls a fair n sided die')
+    async def roll(self, ctx: commands.Context, num_sides: int):
+        """
+        Rolls a n-sided dice.
+        Inputs:
+        n = integer representing the number of sides of the die.
+        """
+        await ctx.send(f"You rolled a {random.randrange(1,num_sides + 1)}")
 
+async def setup(client):
+    await client.add_cog(Misc(client))
