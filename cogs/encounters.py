546d870ead284d4ea28659edbef298dc460a12ea
Alejandro Lopez <alejandrolopez@Alejandros-MacBook-Pro.local>
2023-09-23 23:03:18 -0500

    Made changes

--- cogs/encounters.py
+++ cogs/encounters.py
@@ -9,17 +9,7 @@ class Encounters(commands.Cog):
     @commands.command(name='npc',
                       brief='gives players a prompt from an npc')
     async def npc(self, ctx: commands.Context):
-        await ctx.send(' 
-▄▄▄▄   ▓█████  █     █░ ▄▄▄       ██▀███  ▓█████ 
-▓█████▄ ▓█   ▀ ▓█░ █ ░█░▒████▄    ▓██ ▒ ██▒▓█   ▀ 
-▒██▒ ▄██▒███   ▒█░ █ ░█ ▒██  ▀█▄  ▓██ ░▄█ ▒▒███   
-▒██░█▀  ▒▓█  ▄ ░█░ █ ░█ ░██▄▄▄▄██ ▒██▀▀█▄  ▒▓█  ▄ 
-░▓█  ▀█▓░▒████▒░░██▒██▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▒
-░▒▓███▀▒░░ ▒░ ░░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒▓ ░▒▓░░░ ▒░ ░
-▒░▒   ░  ░ ░  ░  ▒ ░ ░    ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ░  ░
- ░    ░    ░     ░   ░    ░   ▒     ░░   ░    ░   
- ░         ░  ░    ░          ░  ░   ░        ░  ░
-      ░                                           ')
+        await ctx.send(" The party enters the kings crypt, and in front of them stands a large wall, with images of the long history of the kingdom adorning it. Noompar wishes he could enjoy it more, but he's quick to point out the large word painted in fresh blood : \n ",  " ▄▄▄▄   ▓█████  █     █░ ▄▄▄       ██▀███  ▓█████  \n" ,"  ▓█████▄ ▓█   ▀ ▓█░ █ ░█░▒████▄    ▓██ ▒ ██▒▓█   ▀  \n",  " ▒██▒ ▄██▒███   ▒█░ █ ░█ ▒██  ▀█▄  ▓██ ░▄█ ▒▒███   \n",  "  ▒██░█▀  ▒▓█  ▄ ░█░ █ ░█ ░██▄▄▄▄██ ▒██▀▀█▄  ▒▓█  ▄ \n", " ░▓█  ▀█▓░▒████▒░░██▒██▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▒ \n","  ░▒▓███▀▒░░ ▒░ ░░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒▓ ░▒▓░░░ ▒░ ░ \n","    ▒░▒   ░  ░ ░  ░  ▒ ░ ░    ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ░  ░ \n" ,  "    ░    ░    ░     ░   ░    ░   ▒     ░░   ░    ░   \n","     ░         ░  ░    ░          ░  ░   ░        ░  ░ \n",  "     ░          " )
 
 
 
