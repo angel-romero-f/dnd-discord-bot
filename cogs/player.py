480958c289593fc5fecc4e071cf7b7ef39516834
BrianoAden <brianoeaden@gmail.com>
2023-09-23 20:36:08 -0500

    fleshed out player.py

--- cogs/player.py
+++ cogs/player.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import sys
+from Races import Human, Dwarf, Goblin
 
 sys.path.append('DND/')
 sys.path.append('DND/Classes/')
@@ -18,24 +19,28 @@ class Player(commands.Cog):
         self.client = client
 
     @commands.command(name = 'new_char')
-    async def new_char(self, ctx: commands.Context, name: str, class_name: str):
+    async def new_char(self, ctx: commands.Context, name: str, class_name: str, race_name: str):
         self.character_ids[ctx.author] = name
         await ctx.send(f'hello {name}!' )
         class_obj = None
+        race_obj = None
         stat = Stats()
 
-        if class_name.lower() == "bard":
-        
+        if class_name.lower() == "bard":  
             class_obj = Bard()
         elif class_name.lower() == "rogue":
             class_obj = Rogue()
         elif class_name.lower() == "barbarian":
             class_obj = Barbarian()
         
+        if race_name.lower() == 'Human':
+            race_obj = Human()
+        elif race_name.lower() == 'Dwarf':
+            race_obj = Dwarf()
+        elif race_name.lower() == 'Goblin':
+            race_obj = Goblin()
 
-        print('here3')
-        await ctx.send(f"You choose the {class_name} class!")
-        print('here4')
+        await ctx.send(f"You chose the {class_name} class and the {race_name} race!")
         await ctx.send(f"Based on your class of choice, your stats are:")
         try:
             await ctx.send(stat.statroll(class_obj))
@@ -43,13 +48,17 @@ class Player(commands.Cog):
             await ctx.send(f'{e}')
             
 
-        
+    
 
     @commands.command(name = "char_name")
     async def char_name(self, ctx: commands.Context):
         await ctx.send(self.character_ids[ctx.author])
 
-
+def get_character_id(self):
+    """
+    Returns character_id
+    """
+    return self.character_ids
 
 async def setup(client):
     await client.add_cog(Player(client))
